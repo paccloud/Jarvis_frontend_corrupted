98bb07ee0a49c82d6782cdc8e8a7ed116f9a44b4
RyanH <paccloud@gmail.com>
2025-06-28 18:13:35 -0400

    Cool

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
-import React, { useState } from 'react';
 import JarvisAgent from './components/JarvisAgent.jsx';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Jarvis AI Assistant</h1>
-      </header>
-      <main>
-        <JarvisAgent />
-      </main>
-    </div>
+    <JarvisAgent />
   );
 }
 
